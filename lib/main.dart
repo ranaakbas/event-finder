7c57e73576fbd3ae82b4e77262719af314882ff5
Rana Akbas <akbasnar@gmail.com>
2023-03-30 21:43:37 +0300

    firebase connected successfully

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,30 @@
 // import 'dart:ffi';
 // import 'dart:html';
 // import 'dart:ui';
-
+import 'package:firebase_database/firebase_database.dart';
 import 'package:akbas_bas_eventfinderapp/home.dart';
 import 'package:flutter/material.dart';
 import 'package:postgres/postgres.dart';
 import 'package:akbas_bas_eventfinderapp/membership.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FirebaseDatabase database = FirebaseDatabase.instance;
+
+
   runApp(MyApp());
+
 }
 
 class MyApp extends StatelessWidget {
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
