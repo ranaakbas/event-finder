43fd58d5927832210c7050cda2a072b85dc0d2a6
ezonazbass <121170172+ezonazbass@users.noreply.github.com>
2023-04-16 15:21:46 +0300

    Update README.md sign.in sayfası oluşturuldu. şifre gizliliği kullanıcı tarafından ayarlanabilir olarak düzenlendi. şifre ve mail kutucuklarına text eklendi. forgot password kısmı eklendi. kutucuklar arası boşluk azaltıldı.

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@
 import 'package:firebase_database/firebase_database.dart';
 import 'package:akbas_bas_eventfinderapp/home.dart';
 import 'package:flutter/material.dart';
-import 'package:postgres/postgres.dart';
 import 'package:akbas_bas_eventfinderapp/membership.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -26,6 +25,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      
       title: "Event Finder",
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
--- lib/membership.dart
+++ lib/membership.dart
@@ -1,10 +1,23 @@
+import 'dart:ffi';
+import 'dart:math';
+
 import 'package:akbas_bas_eventfinderapp/home.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter/material.dart';
+import 'package:akbas_bas_eventfinderapp/signin.dart';
+import 'package:flutterfire_ui/auth.dart';
 
-class MembershipPage extends StatelessWidget {
-  const MembershipPage({super.key});
+class MembershipPage extends StatefulWidget {
+   MembershipPage({Key? key}) : super(key: key);
+   @override
+  _MembershipPageState createState() => _MembershipPageState();
+}
+
+class _MembershipPageState extends State<MembershipPage> {
+
+  bool _passwordVisible = false;
+
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +39,7 @@ class MembershipPage extends StatelessWidget {
                 margin: EdgeInsets.only(top: 130, bottom: 50),
                 child: Center(
                   child: Text(
-                    "Sign In",
+                    "Log In",
                     style: TextStyle(
                       color: Color(0xFF0A1034),
                       fontSize: 37,
@@ -50,35 +63,96 @@ class MembershipPage extends StatelessWidget {
                       Icons.mail,
                       color: Colors.black,
                     ),
+                    hintText: 'E-Mail',
                   ),
                   style: TextStyle(
                     color: Color(0xFF0A1034),
                   ),
                 ),
               ),
+              
               Container(
                 decoration: BoxDecoration(
                   color: Colors.black12,
                   borderRadius: BorderRadius.circular(20),
                 ),
                 margin:
-                    EdgeInsets.only(top: 30, right: 30, left: 30, bottom: 46),
+                    EdgeInsets.only(top: 10, right: 30, left: 30, bottom: 10),
                 padding:
                     EdgeInsets.only(left: 15, right: 15, top: 3, bottom: 3),
                 child: TextFormField(
-                  obscureText: true,
+                  obscureText: !_passwordVisible,
                   decoration: InputDecoration(
                     border: InputBorder.none,
                     prefixIcon: Icon(
                       Icons.key_rounded,
                       color: Colors.black,
                     ),
+                    hintText: 'Password',
+                    
+                    suffixIcon: GestureDetector(
+        onTap: () {
+          setState(() {
+            _passwordVisible = !_passwordVisible;
+          });
+        },
+        child: Icon(
+          _passwordVisible
+             ? Icons.visibility 
+             : Icons.visibility_off,
+          color: Colors.black,
+        ),
+      ),
                   ),
                   style: TextStyle(color: Color(0xFF0A1034), letterSpacing: 4),
                 ),
               ),
+              Container(
+  margin: EdgeInsets.symmetric(vertical: 10),
+  child: InkWell(
+    onTap: () {},
+    child: Text(
+      "Forgot Password?",
+      style: TextStyle(
+        color: Color(0xFF4F6CC4),
+        fontSize: 16,
+        fontWeight: FontWeight.bold,
+      ),
+      textAlign: TextAlign.center,
+    ),
+  ),
+),
+
               InkWell(
                   onTap: () {},
+                  child: Container(
+                    margin: EdgeInsets.symmetric(horizontal: 100),
+                    width: MediaQuery.of(context).size.width,
+                    height: 45,
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [Color(0xFF4F6CC4), Color(0xFF63AA65)],
+                      ),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Log In",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  )),
+                  SizedBox(height:10),
+              InkWell(
+                
+                  onTap: () { Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return SignInPage();
+    }));
+    },
                   child: Container(
                     margin: EdgeInsets.symmetric(horizontal: 100),
                     width: MediaQuery.of(context).size.width,
@@ -100,6 +174,7 @@ class MembershipPage extends StatelessWidget {
                       ),
                     ),
                   ))
+                    
             ],
           ),
         ),
@@ -129,3 +204,4 @@ Widget buildBackHome(
         ],
       ));
 }
+
