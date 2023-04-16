1bc619ab96ab4ee18459a4ee7c874fde25fb32c8
ezonazbass <121170172+ezonazbass@users.noreply.github.com>
2023-04-16 15:33:44 +0300

    Update README.md sign.in sayfası oluşturuldu. şifre gizliliği kullanıcı tarafından ayarlanabilir olarak düzenlendi. şifre ve mail kutucuklarına text eklendi. forgot password kısmı eklendi. kutucuklar arası boşluk azaltıldı.

---
+++ lib/signin.dart
@@ -0,0 +1,197 @@
+import 'package:akbas_bas_eventfinderapp/home.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
+
+class SignInPage extends StatefulWidget {
+  const SignInPage({Key? key}) : super(key: key);
+
+  @override
+  _SignInPageState createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+
+  bool _passwordVisible = false;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+            color: Colors.white,
+          ),
+          child: ListView(
+            children: [
+              buildBackHome(
+                  backHome: Icons.arrow_back,
+                  widget: HomePage(),
+                  context: context),
+              Container(
+                margin: EdgeInsets.only(top: 130, bottom: 50),
+                child: Center(
+                  child: Text(
+                    "Sign Up",
+                    style: TextStyle(
+                      color: Color(0xFF0A1034),
+                      fontSize: 37,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.black12,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                margin: EdgeInsets.only(left: 30, right: 30, bottom: 10),
+                padding:
+                    EdgeInsets.only(left: 15, right: 15, top: 3, bottom: 3),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    prefixIcon: Icon(
+                      Icons.mail,
+                      color: Colors.black,
+                    ),
+                  hintText: 'E-Mail',
+                  ),
+                  style: TextStyle(
+                    color: Color(0xFF0A1034),
+                  ),
+                ),
+              ),
+               
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.black12,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                margin:
+                    EdgeInsets.only(top: 10, right: 30, left: 30, bottom: 10),
+                padding:
+                    EdgeInsets.only(left: 15, right: 15, top: 3, bottom: 3),
+                child: TextFormField(
+                  obscureText: !_passwordVisible,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    prefixIcon: Icon(
+                      Icons.key_rounded,
+                      color: Colors.black,
+                    ),
+                    
+                    suffixIcon: GestureDetector(
+        onTap: () {
+          setState(() {
+            _passwordVisible = !_passwordVisible;
+          });
+        },
+        child: Icon(
+          _passwordVisible
+             ? Icons.visibility 
+             : Icons.visibility_off,
+          color: Colors.black,
+        ),
+      ),
+      hintText: 'Password',
+                  ),
+                  style: TextStyle(color: Color(0xFF0A1034), letterSpacing: 4),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.black12,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                margin:
+                    EdgeInsets.only(top: 10, right: 30, left: 30, bottom: 10),
+                padding:
+                    EdgeInsets.only(left: 15, right: 15, top: 3, bottom: 3),
+                child: TextFormField(
+                  obscureText: !_passwordVisible,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    prefixIcon: Icon(
+                      Icons.key_rounded,
+                      color: Colors.black,
+                    ),
+                    
+                    suffixIcon: GestureDetector(
+        onTap: () {
+          setState(() {
+            _passwordVisible = !_passwordVisible;
+          });
+        },
+        child: Icon(
+          _passwordVisible
+             ? Icons.visibility 
+             : Icons.visibility_off,
+          color: Colors.black,
+        ),
+      ),
+      hintText: 'Password again',
+                  ),
+                  style: TextStyle(color: Color(0xFF0A1034), letterSpacing: 4),
+                ),
+              ),
+            
+
+
+              InkWell(
+                  onTap: () {},
+                  child: Container(
+                    margin: EdgeInsets.symmetric(horizontal: 100),
+                    width: MediaQuery.of(context).size.width,
+                    height: 45,
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [Color(0xFF4F6CC4), Color(0xFF63AA65)],
+                      ),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Save",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  )),
+                 
+                    
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+Widget buildBackHome(
+    {required IconData backHome,
+    required Widget widget,
+    required BuildContext context}) {
+  return GestureDetector(
+      onTap: () {
+       Navigator.pop(context);
+        },
+      child: Column(
+        children: [
+          SizedBox(height: 29),
+          Icon(
+            backHome,
+            color: Colors.black,
+            size: 50,
+          ),
+        ],
+      ));
+}
+
