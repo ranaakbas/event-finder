720027866cdffd42c80528b02d6e95dca0c11582
ezonazbass <121170172+ezonazbass@users.noreply.github.com>
2023-05-04 21:59:24 +0300

    new

--- lib/main.dart
+++ lib/main.dart
@@ -10,25 +10,20 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:get/get.dart';
 
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options:DefaultFirebaseOptions.currentPlatform,
+    options: DefaultFirebaseOptions.currentPlatform,
   );
-  
 
   runApp(MyApp());
-
 }
 
 class MyApp extends StatelessWidget {
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
       title: "Event Finder",
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
--- lib/search.dart
+++ lib/search.dart
@@ -1,4 +1,3 @@
-import 'dart:ffi';
 import 'package:akbas_bas_eventfinderapp/home.dart';
 import 'package:akbas_bas_eventfinderapp/membership.dart';
 import 'package:akbas_bas_eventfinderapp/music.dart';
@@ -9,6 +8,11 @@ import 'package:akbas_bas_eventfinderapp/art.dart';
 import 'package:akbas_bas_eventfinderapp/sport.dart';
 import 'package:akbas_bas_eventfinderapp/education&more.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:akbas_bas_eventfinderapp/Event1.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
 
 class SearchBar extends StatefulWidget {
   @override
@@ -17,73 +21,101 @@ class SearchBar extends StatefulWidget {
 
 class _SearchBarState extends State<SearchBar> {
   TextEditingController _searchController = TextEditingController();
+  List<dynamic> _events = [];
+
+  Future<void> fetchEvents(String category) async {
+    final response = await http.get(Uri.parse(
+        "https://my-event-api.herokuapp.com/etkinlikler?category=$category"));
+
+    if (response.statusCode == 200) {
+      // API'den veri başarıyla alındı
+      final List<dynamic> events = json.decode(response.body);
+      // events listesini kullanarak istediğiniz işlemleri yapabilirsiniz
+      setState(() {
+        _events = events;
+      });
+      print(events);
+    } else {
+      // API'den veri alınırken bir hata oluştu
+      throw Exception('API isteği başarısız oldu: ${response.statusCode}');
+    }
+  }
+
+  void search(String searchText) {
+    if (searchText.isNotEmpty) {
+      Timer(Duration(seconds: 2), () {
+        fetchEvents(searchText);
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: ListView(children: [
-        Container(
-          margin: EdgeInsets.only(top: 25, left: 25, right: 25),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  SizedBox(
-                    width: 10,
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Flexible(
-                    flex: 1,
-                    child: TextField(
-                      controller: _searchController,
-                      cursorColor: Colors.grey,
-                      decoration: InputDecoration(
-                        fillColor: Colors.black12,
-                        filled: true,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
-                          borderSide: BorderSide.none,
-                        ),
-                        prefixIcon: buildBackHome(icon: Icons.arrow_back, widget: HomePage(), context: context),
-
-                      ),
-                      onSubmitted: (value) {
-                        // Perform search based on input
-                      },
+      body: ListView(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 25, left: 25, right: 25),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    SizedBox(
+                      width: 10,
                     ),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+                Row(
+                  children: [
+                    Flexible(
+                      flex: 1,
+                      child: TextField(
+                        controller: _searchController,
+                        cursorColor: Colors.grey,
+                        decoration: InputDecoration(
+                          fillColor: Colors.black12,
+                          filled: true,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                            borderSide: BorderSide.none,
+                          ),
+                          prefixIcon: Icon(Icons.search, color: Colors.grey),
+                          hintText: "Arama yap",
+                          hintStyle: TextStyle(color: Colors.grey),
+                        ),
+                        onChanged: (searchText) {
+                          search(searchText);
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
-        ),
-      ]),
+          SizedBox(height: 20),
+          _events != null
+              ? ListView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: _events!.length,
+                  itemBuilder: (context, index) {
+                    final event = _events![index];
+                    return ListTile(
+                      title: Text(event['name']),
+                      onTap: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) {
+                          return EventPage(event: event);
+                        }));
+                      },
+                    );
+                  },
+                )
+              : Container(),
+        ],
+      ),
     );
   }
 }
-
-Widget buildBackHome({
-required IconData icon,
-required Widget widget,
-required BuildContext context}) {
-  return GestureDetector(
-  onTap: () {
-Navigator.push(context, MaterialPageRoute(builder: (context) {
-return widget;
-}));
-},
-child: Container(
-  padding: EdgeInsets.all(15),
-  child: Icon(
-  icon,
-  size: 33,
-  color: Color(0xFF0A1034),
-  ),
-  width: 18,
-  ),);
-}
-
