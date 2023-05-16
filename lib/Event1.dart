983e231ddda63c09931ee7db9ab3125fe846ec59
Rana Akbas <akbasnar@gmail.com>
2023-05-16 14:38:26 +0300

    event page design

--- lib/Event1.dart
+++ lib/Event1.dart
@@ -14,144 +14,170 @@ class EventPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: Colors.white,
       body: ListView(
         children: [
           SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  buildBackHome(
-                      backHome: Icons.arrow_back,
-                      widget: HomePage(),
-                      context: context),
-                  SizedBox(height: 24),
-                  Text(
-                    event["name"],
-                    style: TextStyle(
-                      fontSize: 25,
-                      color: Color(0xFF0A1034),
-                      fontWeight: FontWeight.bold,
-                    ),
+            child: Stack(
+              children: [
+                Container(
+                  height: screenHeight * 0.5,
+                  padding: EdgeInsets.symmetric(horizontal: 19, vertical: 20),
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: NetworkImage(event["imageUrl"]),
+                        fit: BoxFit.cover),
                   ),
-                  SizedBox(height: 15),
-                  Container(
-                    height: 350.0,
-                    padding: EdgeInsets.symmetric(horizontal: 19, vertical: 20),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(6),
-                      image: DecorationImage(
-                          image: NetworkImage(event["imageUrl"]),
-                          fit: BoxFit.cover),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    "Place: ${event["place"]}",
-                    style: TextStyle(
-                      fontSize: 18,
-                      color: Color(0xFF0A1034),
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    "City: ${event["city"]}",
-                    style: TextStyle(
-                      fontSize: 18,
-                      color: Color(0xFF0A1034),
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    "Date: ${event["date"]}",
-                    style: TextStyle(
-                      fontSize: 18,
-                      color: Color(0xFF0A1034),
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    "Information",
-                    style: TextStyle(
-                      fontSize: 18,
-                      color: Color(0xFF0A1034),
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Text(
-                    "You have to show your ticket when you're entering the place. You have to older than 18. You couldn't bring any food or drink.",
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Color(0xFF272D4F),
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Text(
-                    "Price: ${event["price"]}",
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Color(0xFF0A1034),
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  InkWell(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => PaymentPage(),
-                        ),
-                      );
-                    },
-                    child: Container(
-                      margin: EdgeInsets.symmetric(horizontal: 100),
-                      width: MediaQuery.of(context).size.width,
-                      height: 45,
-                      padding: EdgeInsets.all(10),
+                ),
+                Column(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(top: screenHeight * 0.4),
                       decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          colors: [
-                            Color(0xFF70B0C5),
-                            Color(0xFF7ACE8C),
-                            Color(0xFFCBBC66),
-                          ],
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(24),
+                          topRight: Radius.circular(24),
                         ),
-                        borderRadius: BorderRadius.circular(20),
                       ),
-                      child: Center(
-                        child: Text(
-                          "Buy Ticket",
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold),
-                        ),
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 16.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                buildBackHome(
+                                    backHome: Icons.arrow_back,
+                                    widget: ,
+                                    context: context,
+                                category: event["category"]),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Text(
+                                  event["name"],
+                                  style: TextStyle(
+                                    fontSize: 29,
+                                    color: Color(0xFF0A1034),
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(height: 15),
+                                Text(
+                                  "Place: ${event["place"]}",
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    color: Color(0xFF0A1034),
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Text(
+                                  "City: ${event["city"]}",
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    color: Color(0xFF0A1034),
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Text(
+                                  "Date: ${event["date"]}",
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    color: Color(0xFF0A1034),
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                Text(
+                                  "Information",
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    color: Color(0xFF0A1034),
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  "You have to show your ticket when you're entering the place. You have to older than 18. You couldn't bring any food or drink.",
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Color(0xFF272D4F),
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 30,
+                                ),
+                                Text(
+                                  "Price: ${event["price"]}",
+                                  style: TextStyle(
+                                    fontSize: 22,
+                                    color: Color(0xFF0A1034),
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(height: 20),
+                                InkWell(
+                                  onTap: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => PaymentPage(),
+                                      ),
+                                    );
+                                  },
+                                  child: Container(
+                                    margin:
+                                        EdgeInsets.symmetric(horizontal: 100),
+                                    width: MediaQuery.of(context).size.width,
+                                    height: 45,
+                                    padding: EdgeInsets.all(10),
+                                    decoration: BoxDecoration(
+                                      gradient: LinearGradient(
+                                        colors: [
+                                          Color(0xFF70B0C5),
+                                          Color(0xFF7ACE8C),
+                                          Color(0xFFCBBC66),
+                                        ],
+                                      ),
+                                      borderRadius: BorderRadius.circular(20),
+                                    ),
+                                    child: Center(
+                                      child: Text(
+                                        "Buy Ticket",
+                                        style: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                ],
-              ),
+                  ],
+                ),
+              ],
             ),
           ),
         ],
@@ -160,24 +186,29 @@ class EventPage extends StatelessWidget {
   }
 }
 
-Widget buildBackHome(
-    {required IconData backHome,
-    required Widget widget,
-    required BuildContext context}) {
+Widget buildBackHome({
+  required IconData backHome,
+  required Widget widget,
+  required BuildContext context,
+  required String category,
+}) {
   return GestureDetector(
-      onTap: () {
-        Navigator.push(context, MaterialPageRoute(builder: (context) {
-          return widget;
-        }));
-      },
-      child: Column(
-        children: [
-          SizedBox(height: 29),
-          Icon(
-            backHome,
-            color: Colors.black,
-            size: 50,
-          ),
-        ],
-      ));
+    onTap: () {
+      Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(builder: (context) => widget),
+            (Route<dynamic> route) => route.isFirst,
+      );
+    },
+    child: Column(
+      children: [
+        SizedBox(height: 29),
+        Icon(
+          backHome,
+          color: Colors.black,
+          size: 50,
+        ),
+      ],
+    ),
+  );
 }
